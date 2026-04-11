12219f56c9a6a87fcd33ece2c35ba26e431c5039
Albert Belbase <albertbelbase018@gmail.com>
2026-04-11 22:21:12 +0545

    Navbar color changed

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className="w-full flex justify-center p-4 sticky top-0 z-[100] bg-white/70 backdrop-blur-md border-b border-gray-100/50">
+        <nav className="w-full flex justify-center p-4 sticky top-0 z-[100] bg-white">
             <div className="flex items-center gap-6 px-6 py-2">
                 {links.map((link) => {
                     const isActive = pathname === link.path;
